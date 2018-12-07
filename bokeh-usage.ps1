eef2330fb4e5be8eede349cfa3f4207f40f1bee7
ogmarko <ogmarko@users.noreply.github.com>
2018-12-07 13:14:25 -0600

    Add files via upload
    Runs the licensing program for PG Bokeh with the license as an argument to get a list of current users who have taken a license. It creates a text file with the users and sends the users to a Database.  A web server on the same server as the Database displays the licensing information as well as who is using it.

---
+++ bokeh-usage.ps1
@@ -0,0 +1,31 @@
+﻿cd "c:\Program Files (x86)\peregrine"
+./rlmutil rlmstat -c "c:\Program Files (x86)\peregrine\peregrinel_carbonvfx_12916_220819.lic" -p bokeh_i -u | out-file  "c:\metrics\temp\bokehusage_temp.txt"
+
+get-content c:\metrics\temp\bokehusage_temp.txt | select -skip 9 | out-file c:\metrics\temp\bokehusage.txt
+
+
+$contents = get-content c:\metrics\temp\bokehusage.txt
+
+remove-item "c:\metrics\bokehusage.txt"
+new-item c:\metrics\bokehusage.txt -type file
+
+
+foreach($line in $contents) {
+    $s = $line -split " "
+    $user = $s[2]
+    If (!(Select-String -Path c:\metrics\bokehusage.txt -pattern $user)){$user + " || " | add-content C:\metrics\bokehusage.txt }
+  
+
+    }
+
+$bokehusage = Get-Content C:\metrics\bokehusage.txt
+$DBConnectionString = "Driver={PostgreSQL UNICODE(x64)};Server=IP;Port=PORT;Database=licenses;Uid=USER;Pwd=PASSWORD;"
+$DBConn = New-Object System.Data.Odbc.OdbcConnection;
+$DBConn.ConnectionString = $DBConnectionString;
+$DBConn.Open();
+$DBCmd = $DBConn.CreateCommand();
+$DBCmd.CommandText = "UPDATE apps SET usage ='" + $bokehusage +"' WHERE product = 'Bokeh (Nuke)'";
+
+$DBCmd.ExecuteReader();
+$DBConn.Close();    
+   
