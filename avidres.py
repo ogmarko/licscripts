600594c4da466190264f4e841b34453684f8aebe
ogmarko <ogmarko@users.noreply.github.com>
2018-12-07 10:21:34 -0600

    Update avidres.py

--- avidres.py
+++ avidres.py
@@ -10,10 +10,6 @@ db = mysql.connector.connect(host="IP",
 
 cur = db.cursor()
 
-x = 0
-respageinfo = []
-resusage = []
-
 driver = webdriver.PhantomJS(service_log_path=os.path.devnull)
 driver.get("http://machine:8888/FneServer/manageReservations_view.action;jsessionid=C418D852835D8AA8669807BB0D6F3D82?page=1")
 
